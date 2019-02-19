609657e141d5f154656ed4c91763bc5fb4cf8dc8
John Ferguson <snobord4life@gmail.com>
2019-02-19 08:16:38 -0700

    added ListView, this needs to be fixed

--- app/src/main/java/com/example/schoolschedulejava/MainActivity.java
+++ app/src/main/java/com/example/schoolschedulejava/MainActivity.java
@@ -1,48 +1,39 @@
 package com.example.schoolschedulejava;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
-
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Cursor cursor = getContentResolver().query(TermProvider.TERMS_URI,
+                DBOpenHelper.TERMS_COLUMNS, null, null, null,
+                null);
+
+        String[] from = {DBOpenHelper.TERM_TITLE};
+        int[] to = {android.R.id.text1};
+        CursorAdapter cA = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1,
+                cursor, from, to, 0);
+        ListView list = findViewById(android.R.id.list);
     }
 
     private void insertTerm(String termTitle, String termStart, String termEnd) {
